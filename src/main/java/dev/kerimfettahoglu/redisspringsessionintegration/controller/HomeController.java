9ede70f93aa265e865181e5e0de8120e5251be8d
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-05-05 23:05:53 +0300

    Dummy web service created.

---
+++ src/main/java/dev/kerimfettahoglu/redisspringsessionintegration/controller/HomeController.java
@@ -0,0 +1,14 @@
+package dev.kerimfettahoglu.redisspringsessionintegration.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("home")
+public class HomeController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello world";
+    }
+}
