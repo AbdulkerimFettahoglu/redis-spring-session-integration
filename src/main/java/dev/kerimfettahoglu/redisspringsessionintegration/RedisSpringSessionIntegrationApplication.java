71a61b86f937a474326917301bb0d3a1f67779f2
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-05-05 22:37:46 +0300

    initial commit

---
+++ src/main/java/dev/kerimfettahoglu/redisspringsessionintegration/RedisSpringSessionIntegrationApplication.java
@@ -0,0 +1,13 @@
+package dev.kerimfettahoglu.redisspringsessionintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringSessionIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSpringSessionIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kerimfettahoglu/redisspringsessionintegration/RedisSpringSessionIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kerimfettahoglu.redisspringsessionintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringSessionIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
